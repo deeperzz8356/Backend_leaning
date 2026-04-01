e9d6c60c86df7c4f1abe0bc9b0ed8f7cd06f6d67
deeperzz8356 <it23.deepkumar.pandey@kccemsr.edu.in>
2026-04-02 01:48:04 +0530

    Add .gitignore file and implement greeting endpoints in FastAPI application

--- fast_api/main.py
+++ fast_api/main.py
@@ -6,3 +6,10 @@ app=FastAPI()
 def read_root():
     return {"Message":"Hello Deep"}
 
+@app.get("/greet")
+def greet():
+    return {"Message":"Welcome to FastAPI"}
+
+@app.get("/greet/{name}")
+def greet_name(name:str, age: Optional[int]= None):
+    return {"Message":f"Good morning {name} and You are {age} years old."}
